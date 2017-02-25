2c263b91ded31aeb76a03314f2c5b8964aad8b4c
Thanat0s <Thanspam@trollprod.org>
2017-02-25 10:23:51 +0100

    modify validators to check json an format, stop on any error

--- validate_all.sh
+++ validate_all.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
+# Check Jsons format, and beautify
+./jq_all_the_things.sh
+rc=$?
+if [[ $rc != 0 ]]; then 
+    exit $rc
+fi
+
 set -e
 set -x
 
-./jq_all_the_things.sh
+# fixme to remove.. 
+# Not need anymore ow, jq stop upon error...
+# diffs=`git status --porcelain | wc -l`
+#
+#if ! [ $diffs -eq 0 ]; then
+#	echo "Please make sure you run ./jq_all_the_things.sh before commiting."
+#	exit
+#fi
 
-diffs=`git status --porcelain | wc -l`
-
-if ! [ $diffs -eq 0 ]; then
-	echo "Please make sure you run ./jq_all_the_things.sh before commiting."
-	exit 1
-fi
-
+# Validate schemas
 for dir in clusters/*.json
 do
   echo -n "${dir}: "
   jsonschema -i ${dir} schema_clusters.json
+  rc=$?
+  if [[ $rc != 0 ]]; then 
+    echo "Error on ${dir}"
+    exit $rc
+  fi
   echo ''
 done
 
@@ -23,6 +36,10 @@ for dir in galaxies/*.json
 do
   echo -n "${dir}: "
   jsonschema -i ${dir} schema_galaxies.json
+  rc=$?
+  if [[ $rc != 0 ]]; then 
+    echo "Error on ${dir}"
+    exit $rc
+  fi
   echo ''
 done
-
