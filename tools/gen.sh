0be0f2ff2863da632136d2a9c4b0a49dc2638e9a
Alexandre Dulaunoy <a@foo.be>
2017-08-16 12:29:17 +0200

    asciidoctor-pdf is now stable

--- tools/gen.sh
+++ tools/gen.sh
@@ -1,6 +1,6 @@
 python3 adoc_galaxy.py >a.txt
 asciidoctor a.txt
-~/git/asciidoctor-pdf/bin/asciidoctor-pdf -a allow-uri-read a.txt
+asciidoctor-pdf -a allow-uri-read a.txt
 cp a.html ../../misp-website/galaxy.html
 cp a.pdf ../../misp-website/galaxy.pdf
 scp a.html circl@cpab.circl.lu:/var/www/nwww.circl.lu/doc/misp-galaxy/index.html
