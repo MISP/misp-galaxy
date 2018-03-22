165c33410d7d649e5bd5fb38be8bd11df2e73d17
StefanKelm <StefanKelm@users.noreply.github.com>
2018-03-22 14:16:06 +0100

    Update create_mitre-enterprise-attack-course-of-action_galaxy.py

--- tools/mitre-cti/v2.0/create_mitre-enterprise-attack-course-of-action_galaxy.py
+++ tools/mitre-cti/v2.0/create_mitre-enterprise-attack-course-of-action_galaxy.py
@@ -6,7 +6,7 @@ import re
 import os
 import argparse
 
-parser = argparse.ArgumentParser(description='Create a couple galaxy/cluster with cti\'s courses-of-action.\nMust be in the mitre/cti/entreprise-attack/course-of-action folder')
+parser = argparse.ArgumentParser(description='Create a couple galaxy/cluster with cti\'s courses-of-action.\nMust be in the mitre/cti/enterprise-attack/course-of-action folder')
 parser.add_argument("-v", "--version", type=int, required=True, help="Version of the galaxy. Please increment the previous one")
 args = parser.parse_args()
 
@@ -27,16 +27,16 @@ for element in os.listdir('.'):
             values.append(value)
 
 galaxy = {}
-galaxy['name'] = "Entreprise Attack - Course of Action"
-galaxy['type'] = "mitre-entreprise-attack-course-of-action"
+galaxy['name'] = "Enterprise Attack - Course of Action"
+galaxy['type'] = "mitre-enterprise-attack-course-of-action"
 galaxy['description'] = "ATT&CK Mitigation"
 galaxy['uuid' ] = "fb5a36c0-1707-11e8-81f5-d732b22a4982"
 galaxy['version'] = args.version
 galaxy['icon'] = "chain"
 
 cluster = {} 
-cluster['name'] = "Entreprise Attack - Course of Action"
-cluster['type'] = "mitre-entreprise-attack-course-of-action"
+cluster['name'] = "Enterprise Attack - Course of Action"
+cluster['type'] = "mitre-enterprise-attack-course-of-action"
 cluster['description'] = "ATT&CK Mitigation"
 cluster['version'] = args.version
 cluster['source'] = "https://github.com/mitre/cti"
@@ -44,8 +44,8 @@ cluster['uuid' ] = "fb870a6a-1707-11e8-b548-17523e4d0670"
 cluster['authors'] = ["MITRE"]
 cluster['values'] = values
 
-with open('generate/galaxies/mitre-entreprise-attack-course-of-action.json', 'w') as galaxy_file:
+with open('generate/galaxies/mitre-enterprise-attack-course-of-action.json', 'w') as galaxy_file:
     json.dump(galaxy, galaxy_file, indent=4)
 
-with open('generate/clusters/mitre-entreprise-attack-course-of-action.json', 'w') as cluster_file:
+with open('generate/clusters/mitre-enterprise-attack-course-of-action.json', 'w') as cluster_file:
     json.dump(cluster, cluster_file, indent=4)
