6e19d21d3aec4bdedcfc9b3a042bb302f340c1ee
Alexandre Dulaunoy <a@foo.be>
2019-03-19 16:49:19 +0100

    chg: [tools] fix the attribution confidence level

--- tools/add_missing_attribution-confidence.py
+++ tools/add_missing_attribution-confidence.py
@@ -16,7 +16,11 @@ with open(args.filename) as json_file:
     for value in data['values']:
         if value.get('meta'):
             if not value.get('meta').get('attribution-confidence') and (value.get('meta').get('cfr-suspected-state-sponsor') or value.get('meta').get('country')):
-                value.get('meta')['attribution-confidence'] = 50
+                value.set('meta')['attribution-confidence'] = "50"
+            elif value.get('meta').get('attribution-confidence')  and (value.get('meta').get('cfr-suspected-state-sponsor') or value.get('meta').get('country')):
+                value.get('meta')['attribution-confidence'] = str(value.get('meta').get('attribution-confidence'))
+
+
 
 with open(args.filename, 'w') as json_file:
     json.dump(data, json_file, indent=2, sort_keys=True, ensure_ascii=False)
