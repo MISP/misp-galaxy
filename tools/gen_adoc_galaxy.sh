0ccbdb862b43de14b62901d051738797485adce2
Alexandre Dulaunoy <a@foo.be>
2020-10-23 11:16:50 +0200

    chg: [tea] first version

--- tools/gen_adoc_galaxy.sh
+++ tools/gen_adoc_galaxy.sh
@@ -3,5 +3,5 @@ asciidoctor -a allow-uri-read a.txt
 asciidoctor-pdf -a allow-uri-read a.txt
 cp a.html ../../misp-website/galaxy.html
 cp a.pdf ../../misp-website/galaxy.pdf
-scp a.html circl@cpab.circl.lu:/var/www/nwww.circl.lu/doc/misp-galaxy/index.html
-scp a.pdf  circl@cpab.circl.lu:/var/www/nwww.circl.lu/doc/misp-galaxy/galaxy.pdf
+scp -l 81920 a.html circl@cpab.circl.lu:/var/www/nwww.circl.lu/doc/misp-galaxy/index.html
+scp -l 81920 a.pdf  circl@cpab.circl.lu:/var/www/nwww.circl.lu/doc/misp-galaxy/galaxy.pdf
