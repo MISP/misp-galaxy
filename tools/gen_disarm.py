b0ebc02b1907b8e9b502a785612af66e777f79c6
Christophe Vandeplas <christophe@vandeplas.com>
2023-12-02 17:59:57 +0100

    new: [disarm] Initial DISARM galaxy #783

---
+++ tools/gen_disarm.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+print("""
+To generate DISARM please:
+cd ../../
+git clone https://github.com/DISARMFoundation/DISARMframeworks.git
+cd DISARMframeworks/CODE
+python3 generate_DISARM_MISP_galaxy.py
+""")
