ee834867b710324f09233cdd175d95d2889a162f
niclas <nicjan01@gmail.com>
2024-02-07 10:34:55 +0100

    Chg [build] dependency check

--- tools/mkdocs/build.sh
+++ tools/mkdocs/build.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
 requirements_path="requirements.txt"
-missing_deps=0
 
-while IFS= read -r line || [[ -n "$line" ]]; do
-    echo "$line" | grep -F -f - <(pip freeze)
-    if [ $? -ne 0 ]; then
-        echo "Missing or incorrect version: $line"
-        ((missing_deps++))
-    fi
-done < "$requirements_path"
+pip freeze > installed.txt
+diff -u <(sort $requirements_path) <(sort installed.txt)
 
-if [ $missing_deps -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo "All dependencies are installed with correct versions."
 else
-    echo "$missing_deps dependencies are missing or have incorrect versions."
+    echo "Dependencies missing or with incorrect versions. Please install all dependencies from $requirements_path into your environment."
+    rm installed.txt # Clean up
     exit 1
 fi
+
+rm installed.txt # Clean up
 
 python3 generator.py
 cd ./site/ || exit
