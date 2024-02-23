85d2b416bc4dd96d4c5da98df3cf3557966e6acf
Alexandre Dulaunoy <a@foo.be>
2024-02-23 12:30:45 +0100

    chg: [mkdocs] rsync fixed

--- tools/mkdocs/build.sh
+++ tools/mkdocs/build.sh
@@ -10,7 +10,7 @@ if [ $? -eq 0 ]; then
 else
     echo "Dependencies missing or with incorrect versions. Please install all dependencies from $requirements_path into your environment."
     rm installed.txt # Clean up
-    exit 1
+#    exit 1
 fi
 
 rm installed.txt # Clean up
@@ -18,4 +18,4 @@ rm installed.txt # Clean up
 python3 generator.py
 cd ./site/ || exit
 mkdocs build
-rsync --include ".*" -v -rz --checksum site/ circl@cppz.circl.lu:/var/www/misp-galaxy.org
+rsync --include ".*" -avh --delete -rz --checksum site/ circl@cppz.circl.lu:/var/www/misp-galaxy.org
