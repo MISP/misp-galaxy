abf230b8537a17dd4bc8a658e7ed0d277939f0fe
Paul Jung (Thanat0s) <Paul.Jung@circl.lu>
2025-12-16 22:13:53 +0100

    fix malpedia output json

--- tools/fetch_malpedia.sh
+++ tools/fetch_malpedia.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 cd "${0%/*}"
 wget -O malpedia.json https://malpedia.caad.fkie.fraunhofer.de/api/get/misp
-mv malpedia.json ../clusters/malpedia.json
+mv malpedia.json ../clusters/malpedia-notfix.json
+# quickfix see : https://github.com/MISP/misp-galaxy/pull/1116
+cat ../clusters/malpedia-notfix.json | egrep -v '[ ][ ][ ][ ]"",' > ../clusters/malpedia.json
+rm ../clusters/malpedia-notfix.json
 ./del_duplicate_refs.py ../clusters/malpedia.json
 ./del_duplicate_uuids.py ../clusters/malpedia.json
 (cd ..; ./jq_all_the_things.sh)
