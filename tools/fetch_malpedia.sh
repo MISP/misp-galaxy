d8dadfb0a1c5af5b86b296d72f6adfef7312b2d2
Paul Jung (Thanat0s) <Paul.Jung@circl.lu>
2025-12-17 08:55:12 +0100

    uppercase please

--- tools/fetch_malpedia.sh
+++ tools/fetch_malpedia.sh
@@ -3,7 +3,7 @@ cd "${0%/*}"
 wget -O malpedia.json https://malpedia.caad.fkie.fraunhofer.de/api/get/misp
 mv malpedia.json ../clusters/malpedia-notfix.json
 # quickfix see : https://github.com/MISP/misp-galaxy/pull/1116
-cat ../clusters/malpedia-notfix.json | grep -ev '[ ][ ][ ][ ]"",' > ../clusters/malpedia.json
+cat ../clusters/malpedia-notfix.json | grep -Ev '[ ][ ][ ][ ]"",' > ../clusters/malpedia.json
 rm ../clusters/malpedia-notfix.json
 ./del_duplicate_refs.py ../clusters/malpedia.json
 ./del_duplicate_uuids.py ../clusters/malpedia.json
