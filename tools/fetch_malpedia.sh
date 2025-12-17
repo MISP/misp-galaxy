2cd723e2f1e4e1502aa29512aff239a87d5cddae
Alexandre Dulaunoy <a@foo.be>
2025-12-17 21:43:35 +0100

    chg: [tools] fixing the malpedia misp output with various scripts

--- tools/fetch_malpedia.sh
+++ tools/fetch_malpedia.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd "${0%/*}"
-wget -O malpedia.json https://malpedia.caad.fkie.fraunhofer.de/api/get/misp
+curl -H 'Authorization: apitoken <TOKEN>' https://malpedia.caad.fkie.fraunhofer.de/api/get/misp >malpedia.json
 mv malpedia.json ../clusters/malpedia.json
 ./del_duplicate_refs.py ../clusters/malpedia.json
 ./del_duplicate_uuids.py ../clusters/malpedia.json
+./del_duplicate_value.py ../clusters/malpedia.json
+./del_empty.py ../clusters/malpedia.json
 (cd ..; ./jq_all_the_things.sh)
